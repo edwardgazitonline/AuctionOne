abc5d29805874818d1bc31806161c05710c8ac4e
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-04-12 01:09:58 +0300

    First commit , How does it work
    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductComponent,
+    SearchComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
